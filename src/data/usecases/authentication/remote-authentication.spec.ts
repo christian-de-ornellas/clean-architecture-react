ac73e7a509e8d0978e32ed9b6c46845d0d589807
Christian Possidonio <christian.possidonio@fdte.io>
2022-09-06 08:59:07 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -2,6 +2,8 @@ import { HttpPostClientSpy } from "@/data/test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 import faker from 'faker'
 import { mockAuthentication } from "@/domain/test/mock-authentication";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-errors";
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
 
 type SutTypes = {
     sut: RemoteAuthentication
@@ -16,19 +18,28 @@ const makeSut = (url = faker.internet.url()):SutTypes => {
 }
 
 describe('RemoteAuthentication', () => {
-    test('Should call HttpPostClient with correct URL ', async() => {
+    test('Should call HttpPostClient with correct URL', async() => {
         const  url = faker.internet.url()
        const {sut, httpPostClientSpy} = makeSut(url)
        await sut.auth(mockAuthentication())
         expect(httpPostClientSpy.url).toBe(url)
     });
 
-    test('Should call HttpPostClient with correct body ', async() => {
+    test('Should call HttpPostClient with correct body', async() => {
        const {sut, httpPostClientSpy} = makeSut()
        const authenticationParams = mockAuthentication()
        await sut.auth(authenticationParams)
         expect(httpPostClientSpy.body).toEqual(authenticationParams)
     });
+
+    test('Should throw InvalidCredentialsError if HttpPostClient returns 401', async() => {
+        const {sut, httpPostClientSpy} = makeSut() 
+        httpPostClientSpy.response = {
+            statusCode: HttpStatusCode.unathorized
+        }      
+        const promise = sut.auth(mockAuthentication())
+        await expect(promise).rejects.toThrow(new InvalidCredentialsError())         
+     });
 
   
     
